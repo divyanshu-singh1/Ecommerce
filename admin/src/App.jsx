b815384eaa89722e11db051b9834706a90a6ed20
Divyanshu <rajsinghrajpoot2002@gmail.com>
2024-07-15 15:43:46 +0530

    Created Admin Panel and also added API's for adding,removing and fetching the products

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Admin />
+    </>
+  )
+}
+
+export default App
---
+++ backend/index.js
@@ -0,0 +1,137 @@
+const port = 4000;
+const express  = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+//Data bse Connectioin with MongoDB 
+mongoose.connect('mongodb+srv://rajsinghrajpoot1:XVK2h5RgaeVHCR4A@cluster0.oxikqrk.mongodb.net/e-commerce');
+
+
+//API Creation
+app.get('/',(req,res)=>{
+    res.send("Express App is running")
+})
+
+
+
+
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+
+const upload = multer({storage: storage});
+
+
+//creating upload endpoint fo rimages
+app.use('/images',express.static('upload/images'))
+app.post('/upload',upload.single('product'),(req,res) =>{
+    console.log('Got post request');
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//Schema for Creating Products
+const Product =  mongoose.model("Product",{
+    id:{
+        type:Number,
+        required: true
+    },
+    name:{
+        type:String,
+        required: true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    new_price:{
+        type:Number,
+        required: true
+    },
+    old_price:{
+        type: Number,
+        required: true
+    },
+    date:{
+        type: Date,
+        default:Date.now
+    },
+    available:{
+        type:Boolean,
+        default: true
+    },
+})
+app.post('/addproduct', async (req,res) => {
+    let products = await Product.find({});
+    let id;
+    
+    if(products.length > 0){
+        console.log(`The length is ${Product.length}`);
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name: req.body.name,
+    })
+})
+
+
+//creating appli for delteing products
+app.post('/removeproduct',async (req,res) =>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log('removed');
+    res.json({
+        success:true,
+        name: req.body.name
+    })
+})
+
+//creating API for getting all Products
+app.get('/allproducts', async (req,res) =>{
+    let products = await Product.find({});
+    console.log("All prodcuts fetched");
+    res.send(products);
+})
+
+
+app.listen(port, (error)=>{
+    if(!error){
+        console.log(`Server running on port: ${port}`);
+    }else{
+        console.log(`Error: ${error}`);
+    }
+})
+
+
